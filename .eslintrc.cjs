50c847affa9546d32405248ef83e22a9893d0a2e
miletalvova <mileta279@gmail.comgit config --global user.name>
2025-03-14 14:59:10 +0100

    new env

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,12 @@
 module.exports = {
   env: {
-    commonjs: true,
     es2021: true,
     node: true,
     jest: true,
   },
   extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 12,
   },
-  rules: { 'prettier/prettier': 'error' },
+  rules: {},
 };
